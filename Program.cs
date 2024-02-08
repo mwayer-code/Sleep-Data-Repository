90a0d78e1b2b6fb1bc82c70b9c534140fdaa31a3
mwayer-code <mwayer@my.wctc.edu>
2024-02-08 14:19:01 -0600

    [1.06] Write data to text file

--- Program.cs
+++ Program.cs
@@ -23,7 +23,8 @@ if (resp == "1")
     DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
         // random number generator
     Random rnd = new Random();
-
+        // create file
+    StreamWriter sw = new StreamWriter("data.txt");
     // loop for the desired # of weeks
     while (dataDate < dataEndDate)
     {
@@ -35,10 +36,11 @@ if (resp == "1")
             hours[i] = rnd.Next(4, 13);
         }
         // M/d/yyyy,#|#|#|#|#|#|#
-        Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+        sw.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
         // add 1 week to date
         dataDate = dataDate.AddDays(7);
     }
+    sw.Close();
 }
 else if (resp == "2")
 {
